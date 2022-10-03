3c378191cb38632b92dbf8e2945374f5ba49bb37
jramso <josue.rsou2@gmail.com>
2022-10-03 10:49:19 -0300

    check

---
+++ Selects.sql
@@ -0,0 +1,4 @@
+-- Active: 1664803088986@@127.0.0.1@5432@jiyrzzup@public
+SELECT * from departamento_projeto inner join projeto on (projeto.numero = departamento_projeto.numero_projeto );
+
+select * from 
